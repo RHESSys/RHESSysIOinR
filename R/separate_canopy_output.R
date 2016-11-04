2d877067b76a5b3ef33f5daaa299613390b1dfd3
ryanrbart <ryanrbart@eri.ucsb.edu>
2016-11-04 13:28:59 -0700

    Generalized separate_canopy_output.R to accept data frame input

--- R/separate_canopy_output.R
+++ R/separate_canopy_output.R
@@ -1,24 +1,20 @@
-#' Separate Canopy Output by Canopy
+#' Separate Canopy Output by Layers
 #'
 #' Function separates RHESSys canopy output by canopy and outputs a data frame that
 #' may be analyzed via \code{ggplot}.
 #'
-#' @param variable Variable of interest from canopy output.
-#' @param dates Date column from canopy output
+#' @param data Data frame with variables that need to be separated.
 #' @param num_canopies Number of canopy (stratum) per patch
 #'
 #' @export
-separate_canopy_output <- function(variable, dates, num_canopies = 2){
+separate_canopy_output <- function(data, num_canopies = 2){
 
-  select_rows <- function(x,y,z) y[seq(x,length(y),z)]
-  variable_by_canopy <- sapply(seq_len(num_canopies), select_rows, y=variable, z=num_canopies, simplify=FALSE)
-  dates_by_canopy <- sapply(seq_len(num_canopies), select_rows, y=dates, z=num_canopies, simplify = FALSE)
-  names_by_canopy <- sapply(seq_len(num_canopies), function(x,y) rep(x,length(y)), variable_by_canopy[[1]], simplify = FALSE)
+  select_rows <- function(x,y,z) y[seq(x,nrow(y),z),]
+  variables_by_canopy <- sapply(seq_len(num_canopies), select_rows, y=data, z=num_canopies, simplify=FALSE)
+  names_by_canopy <- sapply(seq_len(num_canopies), function(x,y) rep(x,nrow(y)), y=variables_by_canopy[[1]], simplify = FALSE)
 
-  tmp <- lapply(seq_len(num_canopies), function(x,w,y,z) data.frame(var=w[[x]],dates=y[[x]],names=z[[x]]), w=variable_by_canopy, y=dates_by_canopy, z=names_by_canopy)
+  tmp <- lapply(seq_len(num_canopies), function(x,y,z) cbind(y[[x]],names=z[[x]]), y=variables_by_canopy, z=names_by_canopy)
   canopy_df <- Reduce(rbind, tmp)
 
   return(canopy_df)
 }
-
-
