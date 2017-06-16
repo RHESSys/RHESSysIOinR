62882dae702079bebd9c2c02a848e2b491fdc3f5
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-06-15 23:38:40 -0700

    Added code to dynamically generate hdr file.

--- R/make_hdr_file.R
+++ R/make_hdr_file.R
@@ -2,31 +2,40 @@
 
 
 
-make_hdr_file <- function(){
+make_hdr_file <- function(master_table, path_initial, num_files, default_file){
 
-  world_hdr_out <- data.frame(c1 = length(hdr_input_list$basin_def), c2 = "num_basin_default_files", stringsAsFactors=FALSE)
+  # Function for assembling path lines for each hdr input
+  def_file_df <- function(def, default_file = "default_file"){
+    output <- lapply(def, function(x) c(x, default_file)) %>%
+      do.call(rbind, .)
+    colnames(output) <- c("c1", "c2")
+    return(output)
+  }
+
+  hdr_out <- data.frame(c1 = length(path_initial), c2 = num_files, stringsAsFactors=FALSE)
 
   path_def_basin = vector()
-  for (zz in seq_along(hdr_input_list$basin_def)){
+  for (zz in seq_along(path_initial)){
 
     # Def file paths and names
-    def_path <- dirname(hdr_input_list$basin_def)
-    name_no_ext <- tools::file_path_sans_ext(basename(hdr_input_list$basin_def))
-    ext <- tools::file_ext(hdr_input_list$basin_def)
+    def_path <- dirname(path_initial)[zz]
+    name_no_ext <- tools::file_path_sans_ext(basename(path_initial))[zz]
+    ext <- tools::file_ext(path_initial)[zz]
     def_path_new <- file.path(def_path, name_no_ext)
 
+    file_name_ext <- master_table[path_initial][zz]
+
     # Determine the type of def file used
-    if (master_hdr_df[yy,hdr_input_list$basin_def][zz] == 0){
+    if (file_name_ext == 0){
       # Reference original def file
-      path_def_basin[zz] <- hdr_input_list$basin_def
+      path_def_basin[zz] <- path_initial
     } else {
       # Reference new def file
-      path_def_basin[zz] <- file.path(def_path, paste(name_no_ext,"_",file_name_ext,".txt",sep=""))
+      path_def_basin[zz] <- file.path(def_path_new, paste(name_no_ext,"_",file_name_ext,".txt",sep=""))
     }
   }
 
-  world_hdr_out <- rbind(world_hdr_out, def_file_df(path_def_basin, "basin_default_file"))
+  hdr_out <- rbind(hdr_out, def_file_df(path_def_basin, default_file))
 
- return(world_hdr_out)
-
+ return(hdr_out)
 }
--- R/process_hdr_def_file.R
+++ R/process_hdr_def_file.R
@@ -7,6 +7,7 @@
 #' Loose ends
 #' Non-unique parameters
 #' Needs to work properly when various components are NULL
+#' Base station code
 #'
 #' @export
 process_hdr_def_file <- function(hdr_input_list, def_par_input_list, dated_seq_input_list, parameter_method, world_hdr_prefix, world_file){
@@ -133,7 +134,7 @@ process_hdr_def_file <- function(hdr_input_list, def_par_input_list, dated_seq_i
   # ---------------------------------------------------------------------
   # Cycle through each base station file
   for (gg in seq_along(base_file_types)){
-    print("not implemented yet")
+    print("Placeholder added")
     master_par_change_df <- cbind(master_par_change_df,  "placeholder_name" = rep(0, length(master_par_change_df[,1])))
     names(master_par_change_df)[names(master_par_change_df) == "placeholder_name"] <- hdr_input_list$base_stations
 
@@ -142,163 +143,68 @@ process_hdr_def_file <- function(hdr_input_list, def_par_input_list, dated_seq_i
   # ---------------------------------------------------------------------
   # Generate hdr file according to master_par_change_df
 
-  # Function for assembling path lines for each hdr input
-  def_file_df <- function(def, default_file = "default_file"){
-    output <- lapply(def, function(x) c(x, default_file)) %>%
-    do.call(rbind, .)
-    colnames(output) <- c("c1", "c2")
-    return(output)
-  }
-
-
+  #Create hdr output folder and path names
+  world_path <- dirname(world_file)
+  world_hdr_path <- file.path(world_path, world_hdr_prefix)
+  if(dir.exists(world_hdr_path) == FALSE){dir.create(world_hdr_path)}
 
   # Select out the def files from master_par_change_df
   master_hdr_df <- master_par_change_df[unlist(hdr_input_list)]
 
-  #Create hdr output folder
-  world_path <- dirname(world_file)
-  world_hdr_path <- file.path(path, world_hdr_prefix)
-  if(dir.exists(world_hdr_path) == FALSE){dir.create(world_hdr_path)}
-
   # Cycle through each parameter set
   for (yy in seq_along(master_hdr_df[,1])){
 
-    # Assemble hdr file
+    # Generate components of hdr file
     if (is.null(hdr_input_list$basin_def)==FALSE){
-
-      world_hdr_out <- data.frame(c1 = length(hdr_input_list$basin_def), c2 = "num_basin_default_files", stringsAsFactors=FALSE)
-
-      path_def_basin = vector()
-      for (zz in seq_along(hdr_input_list$basin_def)){
-
-        # Def file paths and names
-        def_path <- dirname(hdr_input_list$basin_def)
-        name_no_ext <- tools::file_path_sans_ext(basename(hdr_input_list$basin_def))
-        ext <- tools::file_ext(hdr_input_list$basin_def)
-        def_path_new <- file.path(def_path, name_no_ext)
-
-        # Determine the type of def file used
-        if (master_hdr_df[yy,hdr_input_list$basin_def][zz] == 0){
-          # Reference original def file
-          path_def_basin[zz] <- hdr_input_list$basin_def
-        } else {
-          # Reference new def file
-          path_def_basin[zz] <- file.path(def_path, paste(name_no_ext,"_",file_name_ext,".txt",sep=""))
-        }
-      }
-
-      world_hdr_out <- rbind(world_hdr_out, def_file_df(path_def_basin, "basin_default_file"))
+      world_hdr_out <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$basin_def, num_files="num_basin_default_files", default_file="basin_default_file")
     }
 
-    # Repeat
-
-
-
-    rbind(., c(length(hdr_input_list$hill_def), "num_hillslope_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$hill_def, "hillslope_default_file")) %>%
-
-      rbind(., c(length(hdr_input_list$zone_def), "num_zone_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$zone_def, "zone_default_file")) %>%
-
-      rbind(., c(length(hdr_input_list$soil_def), "num_soil_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$soil_def, "soil_default_file")) %>%
-
-      rbind(., c(length(hdr_input_list$landuse_def), "num_landuse_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$landuse_def, "landuse_default_file")) %>%
-
-      rbind(., c(length(hdr_input_list$stratum_def), "num_stratum_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$stratum_def, "stratum_default_file")) %>%
-
-      # Add fire option here
-      rbind(., c(length(hdr_input_list$fire_def), "num_fire_default_files")) %>%
-      rbind(., def_file_df(hdr_input_list$fire_def, "fire_default_file")) %>%
-
-      rbind(., c(length(hdr_input_list$base_stations), "num_base_stations")) %>%
-      rbind(., def_file_df(hdr_input_list$base_stations, "base_stations_file"))
-    #print(world_hdr_out)
-
-
-    world_hdr_name_out <- file.path(world_hdr_path, paste(world_hdr_prefix,"_",yy,".hdr"))
-    # Attach hdr # to master list
-
-
-    write.table(world_hdr_out, file = "world_hdr_name_out", col.names = FALSE, row.names=FALSE, quote = FALSE)
-
-
-
-
-    # for (yy in seq_along(names(hdr_input_list))){
-    #   #names(hdr_input_list)
-    #
-    #   for (zz in seq_along(hdr_input_list[[yy]])){
-    #
-    #     dplyr::select(master_par_change_df, matches(hdr_input_list[[yy]][zz]))
-    #
-    #   }
-    #
-    #   # Print lines of hdr file at this level
-
-
-
+    if (is.null(hdr_input_list$hillslope_def)==FALSE){
+      hdr_hillslope_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$hillslope_def, num_files="num_hillslope_default_files", default_file="hillslope_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_hillslope_comp)
     }
+
+    if (is.null(hdr_input_list$zone_def)==FALSE){
+      hdr_zone_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$zone_def, num_files="num_zone_default_files", default_file="zone_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_zone_comp)
+    }
+
+    if (is.null(hdr_input_list$soil_def)==FALSE){
+      hdr_soil_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$soil_def, num_files="num_soil_default_files", default_file="soil_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_soil_comp)
+    }
+
+    if (is.null(hdr_input_list$landuse_def)==FALSE){
+      hdr_landuse_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$landuse_def, num_files="num_landuse_default_files", default_file="landuse_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_landuse_comp)
+    }
+
+    if (is.null(hdr_input_list$stratum_def)==FALSE){
+      hdr_stratum_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$stratum_def, num_files="num_stratum_default_files", default_file="stratum_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_stratum_comp)
+    }
+
+    if (is.null(hdr_input_list$fire_def)==FALSE){
+      hdr_fire_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$fire_def, num_files="num_fire_default_files", default_file="fire_default_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_fire_comp)
+    }
+
+    if (is.null(hdr_input_list$base_stations)==FALSE){
+      hdr_base_stations_comp <- make_hdr_file(master_table=master_hdr_df[yy,], path_initial=hdr_input_list$base_stations, num_files="num_base_stations_files", default_file="base_stations_file")
+      world_hdr_out <- rbind(world_hdr_out, hdr_base_stations_comp)
+    }
+
+    # Write hdr file
+    world_hdr_name_out <- file.path(world_hdr_path, paste(world_hdr_prefix,"_",yy,".hdr",sep=""))
+    write.table(world_hdr_out, file = world_hdr_name_out, col.names = FALSE, row.names=FALSE, quote = FALSE, sep="        ")
+    print(world_hdr_name_out)
   }
+
+  # Attach hdr # to master list
+  hdr_file_names <- sapply(seq_along(master_hdr_df[,1]), function(x,y,z) file.path(y, paste(z,"_", x,".hdr",sep="")), y=world_hdr_path, z=world_hdr_prefix)
+  master_par_change_df <- cbind(master_par_change_df, hdr_file_names)
+
+  return(master_par_change_df)
 }
-
-
-
-# ----
-
-
-
-
-
-
-
-
-
-
-
-
-
-  #
-  # def_file_desc <- c("basin_default_file","hillslope_default_file","zone_default_file",
-  #                   "soil_default_file","stratum_default_file",
-  #                   "fire_default_file","base_stations_file")
-  #
-  # # Assemble hdr file
-  # hdr_df_tmp <- data.frame(c1 = length(hdr_input_list$basin_def), c2 = "num_basin_default_files", stringsAsFactors=FALSE) %>%
-  #   rbind(., def_file_df(hdr_input_list$basin_def, "basin_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$hill_def), "num_hillslope_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$hill_def, "hillslope_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$zone_def), "num_zone_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$zone_def, "zone_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$soil_def), "num_soil_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$soil_def, "soil_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$landuse_def), "num_landuse_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$landuse_def, "landuse_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$stratum_def), "num_stratum_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$stratum_def, "stratum_default_file")) %>%
-  #
-  #   # Add fire option here
-  #   rbind(., c(length(hdr_input_list$fire_def), "num_fire_default_files")) %>%
-  #   rbind(., def_file_df(hdr_input_list$fire_def, "fire_default_file")) %>%
-  #
-  #   rbind(., c(length(hdr_input_list$base_stations), "num_base_stations")) %>%
-  #   rbind(., def_file_df(hdr_input_list$base_stations, "base_stations_file"))
-  # #print(hdr_df)
-  #
-  # write.table(hdr_df, file = "happy", col.names = FALSE, row.names=FALSE, quote = FALSE)
-  #
-
-
-
-
-
-
 
 
