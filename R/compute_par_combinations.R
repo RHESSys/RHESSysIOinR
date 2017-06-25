a7176f883f71ec594668f32b7c966fad4eb65b39
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-06-24 23:32:53 -0700

    Code for creating data frame of unique parameter sets. Includes all-combinations, monte-carlo and latin hypercube.

---
+++ R/compute_par_combinations.R
@@ -0,0 +1,77 @@
+#' Generates a dataframe of parameter set combinations
+#'
+#' Converts a list of parameter inputs (ranges, values, etc.) to a data frame
+#' containing the specified number of unique parameter set combinations.
+#'
+#' @param par_input_list List of parameters values to be processed. Name of
+#'   parameter should be provided as name of each list component.
+#' @param parameter_method Parameter designating the method to use.
+#'
+#'   Details of all_combinations method...
+#'
+#'   Details of monte-carlo method...
+#'
+#'   Details of latin hypercube method...
+#'
+#'   Details of specific values method...
+#'
+#' @export
+compute_par_combinations <- function(par_input_list, parameter_method){
+
+  # ---------------------------------------------------------------------
+  if (parameter_method == "all_combinations"){
+
+    # Create parameter data frame
+    out <- expand.grid(par_input_list)
+  }
+
+  # ---------------------------------------------------------------------
+  if (parameter_method == "monte_carlo"){
+
+    # Data checks
+    stopifnot(sapply(seq_len(k), function(x,y) length(y[[x]])==3, y=par_input_list)) # Check that there are 3 values for each parameter (presumably a min, max and n)
+    stopifnot(sapply(seq_len(k), function(x,y) y[[x]][3]==y[[1]][3], y=par_input_list)) # Check that total n is equal for all parameters.
+
+    # Inputs
+    k <- length(par_input_list)
+    min_par <- sapply(seq_len(k), function(x,y) y[[x]][1], y=par_input_list)
+    max_par <- sapply(seq_len(k), function(x,y) y[[x]][2], y=par_input_list)
+    n <- par_input_list[[1]][3]
+
+    # Create the parameter data frame
+    out <- mapply(function(x, y, z) runif(min=x, max=y, n=z), x=min_par, y=max_par, MoreArgs = list(z=n))
+    out <- as.data.frame(out)
+    colnames(out) <- names(par_input_list)
+  }
+
+  # ---------------------------------------------------------------------
+  if (parameter_method == "lhc"){   # latin hypercube
+
+    # Data checks
+    stopifnot(sapply(seq_len(k), function(x,y) length(y[[x]])==3, y=par_input_list)) # Check that there are 3 values for each parameter (presumably a min, max and n)
+    stopifnot(sapply(seq_len(k), function(x,y) y[[x]][3]==y[[1]][3], y=par_input_list)) # Check that total n is equal for all parameters.
+
+    # Inputs for Latin Hypercube
+    k <- length(par_input_list)
+    min_par <- sapply(seq_len(k), function(x,y) y[[x]][1], y=par_input_list)
+    max_par <- sapply(seq_len(k), function(x,y) y[[x]][2], y=par_input_list)
+    n <- par_input_list[[1]][3]
+
+    # Transform a Latin hypercube
+    grid <- randomLHS(n, k) # Generate generic hypercube
+    out <- mapply(function(w, x, y, z) qunif(w[,x], min=y, max=z), x=seq_len(k), y=min_par, z=max_par, MoreArgs = list(w=grid))
+    out <- as.data.frame(out)
+    colnames(out) <- names(par_input_list)
+
+    # See KScorrect package, qlunif, for potentially implementing uniform log-normal distribution
+  }
+
+  # ---------------------------------------------------------------------
+  if (parameter_method == "specific_values"){
+
+
+
+  }
+
+ return(out)
+}
