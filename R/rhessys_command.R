a5c5dd8ee6aea42dccc4ba19a379677245923452
ryanrbart <ryanrbart@eri.ucsb.edu>
2017-07-22 07:16:43 -0700

    Updated rhessys_command to accept various combination of parameters.

--- R/rhessys_command.R
+++ R/rhessys_command.R
@@ -6,36 +6,42 @@
 #' include ...
 #'
 #' @param rhessys_version path and file of compiled version of RHESSys.
-#' @param tec_file ???
 #' @param world_file path and file of world_file. World_file is produced using
 #'   grass-to-world
 #' @param world_hdr_file ???
+#' @param tec_file ???
 #' @param flow_file ???
 #' @param start_date ???
 #' @param end_date ???
-#' @param output_folder ???
-#' @param output_filename ???
+#' @param output_file ???
+#' @param input_parameters ???
 #' @param command_options ???
-#' @param m ???
-#' @param k ???
-#' @param m_v ???
-#' @param k_v ???
-#' @param pa ???
-#' @param po ???
-#' @param gw1 ???
-#' @param gw2 ???
-
-
+#'
+#'
 #' @export
-rhessys_command <- function(rhessys_version, tec_file, world_file, world_hdr_file,
-                           flow_file, start_date, end_date, output_folder,
-                           output_filename, command_options,
-                           m, k, m_v, k_v, pa, po, gw1, gw2){
+rhessys_command <- function(rhessys_version,
+                            world_file,
+                            world_hdr_file,
+                            tec_file,
+                            flow_file,
+                            start_date,
+                            end_date,
+                            output_file,
+                            input_parameters,
+                            command_options){
 
-  tmp = sprintf("%s -t %s -w %s -whdr %s -r %s -st %s -ed %s -pre %s -s %f %f -sv %f %f -svalt %f %f -gw %f %f %s",
-                rhessys_version, tec_file, world_file, world_hdr_file, flow_file,
-                start_date, end_date, paste(output_folder,"/",output_filename,sep=""),
-                m, k, m_v, k_v, pa, po, gw1, gw2, command_options)
+  tmp = sprintf("%s -w %s -whdr %s -t %s -r %s -st %s -ed %s -pre %s %s %s",
+                rhessys_version,
+                world_file,
+                world_hdr_file,
+                tec_file,
+                flow_file,
+                start_date,
+                end_date,
+                output_file,
+                input_parameters,
+                command_options)
+
   system(tmp)
 }
 
