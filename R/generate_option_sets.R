7aa805deb4f1d512407084ce0c55772f376a712d
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-09-28 16:38:18 -0700

    Fixing bug in previous commit. This change may allow standard parameters to be used in absence of def parameter changes, though other changes may be necessary

--- R/generate_option_sets.R
+++ R/generate_option_sets.R
@@ -62,6 +62,7 @@ generate_option_sets <- function(parameter_method,
     # Process standard RHESSys parameters
 
     if (is.null(input_standard_par_list[1]) == FALSE){
+      # Scenario where there are standard parameters to process
       option_sets_standard_par <- make_option_set_combinations(input_list=input_standard_par_list, parameter_method=parameter_method)
 
       # Attach group ID to option_sets_standard_par
@@ -69,10 +70,12 @@ generate_option_sets <- function(parameter_method,
       option_sets_standard_par <- dplyr::bind_cols(option_sets_standard_par, data.frame(stan_id = tmp))
 
     } else {
-
+      # Scenario where there are no standard parameters to process
       if (is.null(option_sets_def_par)==FALSE){
+        # If there are def parameters processed, then a option_sets_standard_par dataframe consisting of only the stan_id is made of the same length. The values of 0 represent dummy values.
         option_sets_standard_par <- data.frame(stan_id = rep(0, nrow(option_sets_def_par[[1]])))
       } else {
+        # If there are no def parameters processed, then a option_sets_standard_par dataframe consisting of only the stan_id of length 1 is made. The value of 0 representa a dummy value.
         option_sets_standard_par <- data.frame(stan_id = 0)
       }
     }
@@ -161,23 +164,31 @@ generate_option_sets <- function(parameter_method,
   # ---------------------------------------------------------------------
   # Make table (option_sets_par) for exporting and use in subsequent simulations
 
-  # Isolate variable names from option_sets_def_par
-  tmp1 <- mapply(function(x,y) paste(x, ":", names(y), sep=""),
-                          x=names(option_sets_def_par),
-                          y=option_sets_def_par,
-                          SIMPLIFY = FALSE)
-  tmp2 <- do.call(c, purrr::flatten(tmp1))
-  remove <- grep("group_id", tmp2)
-  names_def_par <- tmp2[-remove]
+  # Isolate variable names from option_sets_def_par. If option_sets_def_par doesn't exist, make names_def_par NULL.
+  if (is.null(option_sets_def_par) == FALSE){
+    tmp1 <- mapply(function(x,y) paste(x, ":", names(y), sep=""),
+                   x=names(option_sets_def_par),
+                   y=option_sets_def_par,
+                   SIMPLIFY = FALSE)
+    tmp2 <- do.call(c, purrr::flatten(tmp1))
+    remove <- grep("group_id", tmp2)
+    names_def_par <- tmp2[-remove]
+  } else {
+    names_def_par=NULL
+  }
 
-  # Isolate variable names from option_sets_standard_par
-  names_standard_par <- option_sets_standard_par %>%
-    names() %>%
-    `[`(. != "stan_id")
+  # Isolate variable names from option_sets_standard_par.  If option_sets_standard_par doesn't exist, make names_def_par NULL.
+  if (ncol(option_sets_standard_par) > 1){
+    # If standard parameters are processed, then option_sets_standard_par will be greater than 1 column.
+    names_standard_par <- option_sets_standard_par %>%
+      names() %>%
+      `[`(. != "stan_id")
+  } else {
+    names_standard_par=NULL
+  }
 
-  #option_sets_par <- dplyr::select(option_sets_all,names_def_par,names_standard_par, all_id) # this is old and threw and error
-
-  option_sets_par <- dplyr::select(option_sets_all, dplyr::one_of(names_def_par,names_standard_par, all_id))
+  # Make a table of parameters along with all_id identifier.
+  option_sets_par <- dplyr::select(option_sets_all,names_def_par,names_standard_par, all_id)
 
   # ---------------------------------------------------------------------
 
