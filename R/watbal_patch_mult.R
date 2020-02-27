38a2b84a31110fba6343af7aecae671084bd67c5
William Burke <burke24@gmail.com>
2020-02-26 20:31:31 -0800

    Added watbal_patch, watbal_patch_mult, and documentation

--- R/watbal_patch_mult.R
+++ R/watbal_patch_mult.R
@@ -1,7 +1,7 @@
-#' watbal_patch.R
+#' watbal_patch_mult.R
 #'
-#' Water balance for a single patch daily RHESSys output.
-#'
+#' Water balance for multiple patches daily RHESSys output. 
+#' 
 #' Before you run the water balance script below, you must have added a "rain" column to your patch daily output
 #' data that contains the patch-specific precipitation. Be careful to add the right precip values if your worldfile
 #' uses multiple base stations, isohyets, or a precip lapse rate.
@@ -12,51 +12,32 @@
 #'
 #' If you have multiple patches with only a single stratum per patch, you need to run a different script to loop
 #' through patches individually, call the water balance function, then write the output. For example, if your
-#' patch output is called "rp", your canopy output is called "rc", and you want the water balance output written
-#' to atable called "out", then you would type: "out=pwatbalmult(rp,rc)". This creates a new output table, called
+#' patch output is called "pd", your canopy output is called "cd", and you want the water balance output written
+#' to atable called "out", then you would type: "out=pwatbalmult(pd,cd)". This creates a new output table, called
 #' "out" in this example, that contains date fields and a single column for each patch with the water balance error
 #' for that patch (called "P345","P578","P900", etc. where the field label number matches the patch ID). You might
 #' see a minor numerical error here even if your water balances (on the order of 10^-6). If your watbal values are
 #' negative then water inputs are less than wateroutputs and vice versa. Note that this may take a long time to run
 #' if you have many patches; better to run on a single patch or a single hillslope than a full basin.
 #'
-#' See `watbal_patch_mult` for multiple patches.
-#'
 #' @param pd Patch daily rhessys output, read into R with a funciton like `readin_rhessys_output()`
 #' @param cd Canopy daily rhessys output, read into R with a funciton like `readin_rhessys_output()`
 #'
 #' @export
 
-watbal_patch = function(pd, cd) {
-
-  library(tidyverse)
-  pd$watbal.tmp=with(pd,rain+Qin-Qout-trans_sat-trans_unsat-evap-evap_surface-soil_evap)
-  pd$sd=with(pd,sat_def-rz_storage-unsat_stor)
-
-  cd$weighted_snow_stored = cd$snow_stored * cd$covfrac
-  cd$weighted_rain_stored = cd$rain_stored * cd$covfrac
-  tmp = cd %>% group_by(zoneID, hillID, basinID, patchID, day, month, year) %>% summarize(can_snow_stored = sum(weighted_snow_stored), can_rain_stored = sum(weighted_rain_stored) )
-
-  pd = left_join(pd, tmp[,c("basinID","hillID","zoneID","patchID","day","month","year","can_snow_stored","can_rain_stored")])
-
-  pd$can_water_stored = pd$can_rain_stored+pd$can_snow_stored
-
-  tmp=diff(pd$sd)
-  tmp=c(0,tmp)
-  pd$sddiff=tmp
-  tmp=diff(pd$snow)
-  tmp=c(0,tmp)
-  pd$snodiff=tmp
-  tmp=diff(pd$detention_store)
-  tmp=c(0,tmp)
-  pd$detdiff=tmp
-  tmp=diff(pd$litter.rain_stor)
-  tmp=c(0,tmp)
-  pd$litdiff=tmp
-  tmp=diff(pd$can_water_stored)
-  tmp=c(0,tmp)
-  pd$candiff=tmp
-  pd$watbal=with(pd,watbal.tmp+sddiff-snodiff-detdiff-litdiff-candiff)
-  pd$watbal[1]=0.0
-  return(pd)
+watbal_patch_mult = function(pd, cd) {
+  pids=unique(pd$patchID)
+  tmp=subset(pd,pd$patchID==pids[1])
+  wbp=tmp[c("day","month","year")]
+  wbp=mkdate(wbp)
+  n=ncol(wbp)+1
+  for (i in pids) {
+    tmpp=subset(pd,pd$patchID==i)
+    tmpc=subset(cd,cd$patchID==i)
+    tmpp=watbal_patch(tmpp,tmpc)
+    wbp[,n]=tmpp$watbal
+    names(wbp)[c(n)]=paste("P",i,sep="")
+    n=n+1
+  }
+  return(wbp)
 }
